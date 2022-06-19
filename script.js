3203d7908f7156758759caf25f7a67b64f40e489
Yui-Iida <yxuxi328@icloud.com>
2022-06-19 16:49:58 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const fetchRandomUser = async () => {
+  //
+  for (var i = 0; i < 10; i++) {
+    try {
+      let response = await fetch('https://randomuser.me/api/');
+      let parsedData = await response.json();
+      console.log(parsedData);
+
+      const img = parsedData.results[0].picture.large;
+      const name = parsedData.results[0].name;
+      const fullName = `${name.first} ${name.last}`.toLowerCase();
+      const nat = parsedData.results[0].nat.toLowerCase();
+
+      const user = document.querySelector('#user');
+      user.insertAdjacentHTML(
+        'afterbegin',
+        `
+      <img src=${img} class="img"/>
+      <p>${fullName}</p>
+      <span class="flag-icon flag-icon-${nat}"></span>
+      <div id="icons">
+      <i class="fa fa-brands fa-github"></i>
+      <i class="fa fa-brands fa-facebook-f"></i>
+      <i class="fa fa-brands fa-twitter"></i>
+    </div>
+      `
+      );
+
+      //   // to display image
+      //   const displayImg = document.querySelector('#img');
+      //   const img = parsedData.results[0].picture.large;
+      //   displayImg.insertAdjacentHTML(
+      //     'afterend',
+      //     `<img src=${img} class="img"/>
+
+      //     `
+      //   );
+
+      //   // to display name
+      //   const displayName = document.querySelector('#name');
+      //   const name = parsedData.results[0].name;
+      //   const fullName = `${name.first} ${name.last}`.toLowerCase();
+      //   displayName.insertAdjacentHTML('afterend', `${fullName}`);
+
+      //   // to display flag
+      //   const displayCountry = document.querySelector('#flag');
+      //   // const country = parsedData.results[0].location.country;
+      //   const nat = parsedData.results[0].nat.toLowerCase();
+      //   // displayCountry.insertAdjacentHTML('afterend', `${country}`);
+      //   console.log(nat);
+      //   displayCountry.insertAdjacentHTML(
+      //     'afterend',
+      //     `<span class="flag-icon flag-icon-${nat}"></span>`
+      //   );
+      // to change color depending on gender
+      const icon = document.querySelector('#icons');
+      const gender = parsedData.results[0].gender;
+      if (gender === 'male') icon.style.color = 'red';
+      else icon.style.color = 'green';
+    } catch (error) {
+      console.log('Error!: ', error);
+    }
+  }
+};
+fetchRandomUser();
