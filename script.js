a83cf43dde1744324a0b2bc6933ab9187ff5271d
Yui-Iida <yxuxi328@icloud.com>
2022-06-19 17:29:05 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const fetchRandomUser = async () => {
   //
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 32; i++) {
+    let response = await fetch('https://randomuser.me/api/');
+    let parsedData = await response.json();
+    // console.log(parsedData);
     try {
-      let response = await fetch('https://randomuser.me/api/');
-      let parsedData = await response.json();
-      console.log(parsedData);
-
       const img = parsedData.results[0].picture.large;
       const name = parsedData.results[0].name;
       const fullName = `${name.first} ${name.last}`.toLowerCase();
       const nat = parsedData.results[0].nat.toLowerCase();
 
-      const user = document.querySelector('#user');
-      user.insertAdjacentHTML(
+      const users = document.querySelector('#users');
+      users.insertAdjacentHTML(
         'afterbegin',
         `
+        
+      <div class="user">
       <img src=${img} class="img"/>
       <p>${fullName}</p>
       <span class="flag-icon flag-icon-${nat}"></span>
@@ -22,6 +23,7 @@ const fetchRandomUser = async () => {
       <i class="fa fa-brands fa-github"></i>
       <i class="fa fa-brands fa-facebook-f"></i>
       <i class="fa fa-brands fa-twitter"></i>
+    </div>
     </div>
       `
       );
@@ -62,4 +64,5 @@ const fetchRandomUser = async () => {
     }
   }
 };
+
 fetchRandomUser();
